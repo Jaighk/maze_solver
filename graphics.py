97c1102b71efe2f325c3d8218cc3e7a7449cd6a4
jaighk <jacob.parker51@gmail.com>
2025-04-14 12:10:56 -0500

    implemented line drawing

--- graphics.py
+++ graphics.py
@@ -5,7 +5,7 @@ class Window:
         self.__root = Tk()
         self.__root.title("maze solver")
         self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
-        self.__canvas.pack(fill=BOTH, expand=1)
+        self.__canvas.pack(fill=BOTH, expand=0)
         self.running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
@@ -18,12 +18,23 @@ class Window:
         while self.running:
             self.redraw()
 
+    def draw_line(self, line , fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+
     def close(self):
         self.running = False
 
-def main():
-    window = Window(800, 600)
-    window.wait_for_close()
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-if __name__ == "__main__":
-    main()
+class Line:
+    def __init__(self, point_one, point_two):
+        self.point_one = point_one
+        self.point_two = point_two
+
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(
+            self.point_one.x, self.point_one.y, self.point_two.x, self.point_two.y, fill=fill_color, width=2
+        )
---
+++ main.py
@@ -0,0 +1,10 @@
+from graphics import *
+
+def main():
+    window = Window(800, 600)
+
+    # Keep the window open
+    window.wait_for_close()
+
+if __name__ == "__main__":
+    main()
