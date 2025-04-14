26355febbfd6bdd90e2ca3aa4e6c25d273ac9013
jaighk <jacob.parker51@gmail.com>
2025-04-14 11:32:10 -0500

    fixed Window class to properly use canvas

--- maze_sovler.py
+++ maze_sovler.py
@@ -2,18 +2,16 @@ from tkinter import Tk, BOTH, Canvas
 
 class Window:
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.root = Tk()
-        self.root.title("maze solver")
-        self.root.protocol("WM_DELETE_WINDOW", self.close)
-        self.canvas = Canvas()
-        self.canvas.pack()
+        self.__root = Tk()
+        self.__root.title("maze solver")
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__canvas.pack(fill=BOTH, expand=1)
         self.running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
     def redraw(self):
-       self.root.update_idletasks()
-       self.root.update()
+       self.__root.update_idletasks()
+       self.__root.update()
 
     def wait_for_close(self):
         self.running = True
